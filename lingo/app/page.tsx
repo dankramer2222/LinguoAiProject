6fe25bef9a5d0b0aacf6565a5b7c4c8f78f0b810
Danila Albutov <danil.miheev2222@gmail.com>
2024-03-30 17:07:04 -0700

    init project + implemented library with buttons

---
+++ lingo/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <p className="text-green-500 font-bold text-xs"> 
+        <Button size="lg">Click me </Button>
+      </p>
+    </div>
+  )
+}
