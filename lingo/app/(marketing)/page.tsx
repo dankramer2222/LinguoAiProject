ad314be19b88bed07c5601be53d5eedafd604646
Danila Albutov <danil.miheev2222@gmail.com>
2024-03-31 00:04:10 -0700

    add header and footer

--- lingo/app/(marketing)/page.tsx
+++ lingo/app/(marketing)/page.tsx
@@ -2,10 +2,12 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div>
+    <div className="min-h-screen flex flex-coll">
+      <main className=" flex-1 flex flex-col items-center justify-center">
       <p className="text-green-500 font-bold text-xs"> 
         <Button size="lg">Click me </Button>
       </p>
+      </main>
     </div>
   )
 }
